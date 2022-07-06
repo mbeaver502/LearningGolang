f59ef6a5ab8e9cf5f265497161559e750fd5c55b
Michael Beaver <jmbeaver1@gmail.com>
2022-07-05 21:00:08 -0500

    Solution to Exercise #9

---
+++ 008_exercises/009_exercise9/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		"bond_james":      {"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": {"James Bond", "Literature", "Computer Science"},
+		"no_doctor":       {"Evil", "Ice Cream", "Sunsets"},
+	}
+
+	m["q_agent"] = []string{"Gadgets", "Guns"}
+
+	fmt.Println(m)
+
+	for k, v := range m {
+		fmt.Println(k)
+
+		for i, x := range v {
+			fmt.Println(i, x)
+		}
+	}
+}
