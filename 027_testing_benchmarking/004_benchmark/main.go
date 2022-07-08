f9c6fd802441f5783dbddd552f2121658eb4b77a
Michael Beaver <jmbeaver1@gmail.com>
2022-07-08 15:40:32 -0500

    Benchmarking

---
+++ 027_testing_benchmarking/004_benchmark/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mbeaver502/LearningGolang/027_testing_benchmarking/004_benchmark/saying"
+)
+
+func main() {
+	fmt.Println(saying.Greet("James"))
+}
